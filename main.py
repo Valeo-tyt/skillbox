f51b99040810616e1fa454696e6c0f7422dd9b42
Valentin_V <V.Vorobyev@A1.by>
2023-11-16 11:37:09 +0300

    files in dir

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+from art import tprint
+
 from list_2_5 import f1, f2, f3, f4, f5, f6
-from list_3_7 import f7, f8, f9, f10, f11, f12, f13, f14
 
-# На соседних листах лежит решения. модуль 2.5 был для самопроверки
-# По необходимости можно выводить результат от f7 до f14
+from module3.task_1 import f1
+from module3.task_2 import f2
+from module3.task_3 import f3
+from module3.task_4 import f4
+from module3.task_5 import f5
+from module3.task_6 import f6
+from module3.task_7 import f7
+from module3.task_8 import f8
 
-f14()
+tprint("Skillbox")
+# По необходимости можно выводить название функции от f1 до f8 для проверки результата
+
+f8()

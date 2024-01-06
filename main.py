0a339e8bec8af8f3d9787714334845d40a2b6594
Valentin_V <V.Vorobyev@A1.by>
2024-01-06 16:54:48 +0300

    new task_4.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from art import tprint
 
 # from list_2_5 import f1, f2, f3, f4, f5, f6
-from module5.task_3 import f3
+from module5.task_4 import f4
 
 tprint("Skillbox")
 
 # По необходимости можно выводить название функции для проверки результата
-f3()
+f4()

eeb170fc5697fd94be958a711f1dc26ec57a0dd2
Valentin_V <V.Vorobyev@A1.by>
2024-01-07 18:57:05 +0300

    task_1.py

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
 from art import tprint
 
-from module5.task_1 import f1
-from module5.task_2 import f2
-from module5.task_3 import f3
-from module5.task_4 import f4
-from module5.task_5 import f5
-from module5.task_6 import f6
-from module5.task_7 import f7, f8
+from module6.task_1 import f1
 
 tprint("Skillbox")
 
 # По необходимости можно выводить название функции для проверки результата
 
-# f1()
-# f2()
-# f3()
-# f4()
-# f5()
-# f6()
-# f7() # Вариант 1
-f8()  # Вариант 2
+f1()
---
+++ module6/task_1.py
@@ -0,0 +1,6 @@
+def f1():
+    num = int(input('Введите число: '))
+    count = 1
+    while count <= 3:
+        print(num ** count)
+        count += 1

04e1dd3c034eca6ddb23420c4276ec634716a431
Valentin_V <V.Vorobyev@A1.by>
2024-01-07 16:16:06 +0300

    fix main page

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from art import tprint
 
+from module5.task_1 import f1
+from module5.task_2 import f2
+from module5.task_3 import f3
+from module5.task_4 import f4
+from module5.task_5 import f5
+from module5.task_6 import f6
 from module5.task_7 import f7, f8
 
 tprint("Skillbox")
 
 # По необходимости можно выводить название функции для проверки результата
 
+# f1()
+# f2()
+# f3()
+# f4()
+# f5()
+# f6()
 # f7() # Вариант 1
 f8()  # Вариант 2

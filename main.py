6effbc9c9489f73632e1334cf9ab019aebc81f4c
Valentin_V <V.Vorobyev@A1.by>
2024-01-08 23:16:08 +0300

    task_5.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from module6.task_1 import f1
 from module6.task_2 import f2
 from module6.task_3 import f3
 from module6.task_4 import f4
+from module6.task_5 import f5
 
 
 tprint("Skillbox")
@@ -13,6 +14,7 @@ tprint("Skillbox")
 # f1()
 # f2()
 # f3()
-f4()
+# f4()
+f5()
 
 

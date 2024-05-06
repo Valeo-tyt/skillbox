53242562f923f9e1f7020c44d8e948733dc36c66
Replit user <>
2024-05-06 20:45:39 +0000

    new main page

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from art import tprint
 
-from src.modules.task_2 import *
-from src.modules.task_3 import *
-from src.modules.task_4 import *
-from src.modules.task_5 import *
-from src.modules.task_6 import *
+# from src.modules.task_2 import *
+# from src.modules.task_3 import *
+# from src.modules.task_4 import *
+# from src.modules.task_5 import *
+from src.modules.task_6 import f6_8
 
 # Для проверки результата вызывайте функцию из файла модуля
 
